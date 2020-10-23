acc4f9f04bd14fcca44169fdfbdea9246f6f0480
Micchili <wot.dannbo@gmail.com>
2020-10-23 11:34:37 +0900

    firebase initの処理を変更

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import firebase from "firebase/app"
-
+import firebaseConfig from "./firebaseConfig.json"
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
