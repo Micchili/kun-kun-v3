296b4381a3f9678c2bf5ed60f50b8daadbb0cc71
Micchili <wot.dannbo@gmail.com>
2020-11-06 10:59:06 +0900

    FireBase関連を削除

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import firebase from "firebase/app"
-import firebaseConfig from "./firebaseConfig.json"
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 
 ReactDOM.render(
     <App />,
