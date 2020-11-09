8d30329f9959813c1cf59424500f1cc777f812c3
Micchili <wot.dannbo@gmail.com>
2020-11-09 12:32:06 +0900

    Camera.jsxを表示

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import 'firebase/storage';
-import Sample from "./components/sample"
+import Camera from './components/camera'
 
 export const App = () => {
   return (
-    <Sample />
+    <Camera />
   );
 }
 
