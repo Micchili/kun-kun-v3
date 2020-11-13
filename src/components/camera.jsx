072d01d42f8c6a79ef3cb1cf785874f540ea6862
Micchili <wot.dannbo@gmail.com>
2020-11-13 10:06:24 +0900

    ローカルとサーバーで送信ボタンを区別

--- src/components/camera.jsx
+++ src/components/camera.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import Webcam from "react-webcam";
-
 import axios from 'axios';
 
 class Camera extends React.Component {
@@ -18,7 +16,6 @@ class Camera extends React.Component {
         const imageSrc = this.webcam.getScreenshot();
         var img = new Image();
         img.src = imageSrc;
-        // img.src = this.state.imageSrc;
 
         var canvas = document.getElementById("canvassample");
         var canvasdraw = canvas.getContext("2d");
@@ -26,7 +23,6 @@ class Camera extends React.Component {
             canvasdraw.drawImage(img, 0, 0, 1280, 720);
         }
         this.canvasBinary(canvas);
-        //alert(imageSrc);
     };
 
     canvasBinary = (canvas) => {
@@ -40,38 +36,10 @@ class Camera extends React.Component {
         this.setState({
             imageVinary: buffer,
         });
-        // return buffer;
     }
 
-    // postman = () => {
-    //     const url = "https://localhost:3001";
-
-    //     axios.post(url, this.state.imageVinary)
-    //     .then(function(response) {
-    //         // 成功時
-    //         console.log(response.data);
-    //     })
-    //     .catch(function(error) {
-    //         // エラー時
-    //         console.log(error);
-    //     });
-
-    //     axios.get(url)
-    //     .then(function(response){
-    //         console.log(response.data);
-    //     })
-    //     .catch(function(error){
-    //         console.log(error);
-    //     })
-    // };
-
-
-    postman = () => {
-
-        const subscriptionKey = process.env.REACT_APP_FACE_KEY;
-
-        const endpoint = process.env.REACT_APP_FACE_URL;
-
+    servePostman = () => {
+        const url = "https://localhost:4000";
         const canvas = document.getElementById("canvassample");
 
         const base64 = canvas.toDataURL('image/jpeg', 1.0)
@@ -84,17 +52,48 @@ class Camera extends React.Component {
         // Blobを作成
         var blob = new Blob([buffer.buffer]);
 
+        axios({
+            method: 'post',
+            url: url,
+            data: blob,
+            headers: {
+                "Content-Type": "application/octet-stream"
+            }
+        }).then(function (res) {
+            console.log('Status text: ' + res.status)
+            console.log('Status text: ' + res.statusText)
+            res.data.forEach((face) => {
+                const resData = JSON.stringify(face.faceAttributes.emotion)
+                console.log('Emotion: ' + resData)
+            });
+        }).catch(function (error) {
+            console.log(error)
+        });
+    };
 
-        console.log(blob);
+
+    localPostman = () => {
+
+        const subscriptionKey = process.env.REACT_APP_FACE_KEY;
+        const endpoint = process.env.REACT_APP_FACE_URL;
+        const canvas = document.getElementById("canvassample");
+
+        const base64 = canvas.toDataURL('image/jpeg', 1.0)
+        // Base64からバイナリへ変換
+        var bin = atob(base64.replace(/^.*,/, ''));
+        var buffer = new Uint8Array(bin.length);
+        for (var i = 0; i < bin.length; i++) {
+          buffer[i] = bin.charCodeAt(i);
+        }
+        // Blobを作成
+        var blob = new Blob([buffer.buffer]);
 
         axios({
             method: 'post',
             url: endpoint,
-
             params: {
                 returnFaceAttributes: 'emotion'
             },
-
             data: blob,
             headers: {
                 'Ocp-Apim-Subscription-Key': subscriptionKey ,
@@ -130,7 +129,8 @@ class Camera extends React.Component {
                     videoConstraints={videoConstraints}
                 /><br />
                 <button onClick={this.capture}>base64を表示する</button>
-                <button onClick={this.postman}>送信</button>
+                <button onClick={this.localPostman}>ローカルで送信</button>
+                <button onClick={this.servePostman}>サーバーに送信</button>
 
                 <noscript>You need to enable JavaScript to run this app.</noscript>
                 <div id="root"></div>
