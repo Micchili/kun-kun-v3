771d714e1d74edf054721776d5b6fa04bc421112
Micchili <wot.dannbo@gmail.com>
2020-11-18 15:04:12 +0900

    文字をマテリアルっぽく変更

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 const H1 = styled.h1`
+    font-family: 'Open Sans', sans-serif;
+    will-change: transform, opacity;
+    text-shadow: 0px 5px 40px #00000020, 0px 5px 5px #00000030;
+    font-weight: 700;
+
     @media screen and (min-width: 1024px) {
-    font-size: 2.4rem;
+        font-size: 10rem;
     }
     @media screen and (min-width: 768px) and (max-width: 1024px) {
         font-size: 2rem;
