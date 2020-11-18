a8f1f39c011a9d63e6e28796f7539e4ae85471bc
Micchili <wot.dannbo@gmail.com>
2020-11-18 15:02:19 +0900

    Mainコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Camera from './components/camera'
+import { Main } from './components/Main'
 import {Home} from './pages/Home'
 import * as Styled from './Global.Reset'
 import Camera from './components/camera'
-
 export const App = () => {
 
   return (
     <>
       <Styled.Global />
       {/* <Header /> */}
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/camera" component={Camera} />
-      </Router>
+      <Main>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/camera" component={Camera} />
+        </Router>
+      </Main>
     </>
   );
 }
